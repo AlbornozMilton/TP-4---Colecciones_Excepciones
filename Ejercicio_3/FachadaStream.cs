289808b579ec159b48a1e21eb6923e2a88767f81
MauriChamorro <mauricio-chamorro@hotmail.com>
2016-10-21 21:50:43 -0300

    Stream v1

---
+++ Ejercicio_3/FachadaStream.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Ejercicio_3
+{
+    class FachadaStream
+    {
+        private StreamReader iStreamR;
+
+        public string Ruta(string pRuta)
+        {
+            try
+            {
+                this.iStreamR = new StreamReader(@pRuta);
+                return this.iStreamR.ReadToEnd();
+            }
+            catch (ArgumentException)
+            {
+                throw new Exception("Ingrese alguna Ruta");
+            }
+            catch (FileNotFoundException)
+            {
+                return "Archivo Inexistente";
+            }
+            catch (DirectoryNotFoundException)
+            {
+                return "Dirección Invalida";
+            }
+            
+            finally
+            {
+                if (this.iStreamR == null) this.iStreamR.Close();
+                else this.iStreamR.Close();
+            }
+            
+        }
+    }
+}
---
+++ Ejercicio_3/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            FachadaStream iFachada = new FachadaStream();
+
+            Console.WriteLine("Ingrese ruta de archivo:");
+            string mRuta = Console.ReadLine();
+
+            Console.WriteLine(iFachada.Ruta(mRuta));
+
+            Console.WriteLine("----------FIN-----------");
+
+            Console.ReadKey();
+        }
+    }
+}
