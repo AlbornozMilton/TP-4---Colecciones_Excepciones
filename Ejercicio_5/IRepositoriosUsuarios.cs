d682efa44ea321c22148aa97ca29d7c8cc92ccf1
MauriChamorro <mauricio-chamorro@hotmail.com>
2016-10-28 19:08:14 -0300

    Inicio ejercicio 5

---
+++ Ejercicio_5/IRepositoriosUsuarios.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio_5
+{
+    interface IRepositoriosUsuarios: IList<Usuario> //o IComaprer<>
+    {
+        void Agregar(Usuario pUsuario);
+        void Actualizar(Usuario pUsuario);
+        void Eliminar(string pCodigo);
+        IList<Usuario> ObtenerTodos();
+        Usuario ObtenerPorCodigo(string pCodigo);
+        IList<Usuario> ObtenerOrdenadoPor(); //agregar parametro iComparer<>
+        
+    }
+}
---
+++ Ejercicio_5/Usuario.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio_5
+{
+    class Usuario
+    {
+        private string iCodigo { get; set; }
+        private string iNombreCompleto { get; set; }
+        private string iCorreoElectronico { get; set; }
+
+    }
+}
