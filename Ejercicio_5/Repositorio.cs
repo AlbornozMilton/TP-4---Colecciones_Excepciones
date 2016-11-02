c4e41afb00befcf6ec60940796dafab3aa0c1fd0
MauriChamorro <mauricio-chamorro@hotmail.com>
2016-11-02 17:55:49 -0300

    Implementado Interface IRepo

--- Ejercicio_5/Repositorio.cs
+++ Ejercicio_5/Repositorio.cs
@@ -6,7 +6,45 @@ using System.Threading.Tasks;
 
 namespace Ejercicio_5
 {
-    class Repositorio
+    class Repositorio :IRepositoriosUsuarios
     {
+        IDictionary<String, Usuario> iEvaluadoresPorCliente = new Dictionary<String, Usuario>(); //ver si es mejor Dictionary/SortedDictionary/SortedList
+
+        public Repositorio()
+        {
+            //cargar intancias de usuarios <codigo,usuario>
+        }
+
+
+        public void AgregarUsuario(Usuario pUsuario)
+        {
+
+        }
+
+        public void Actualizar(Usuario pUsuario)
+        {
+
+        }
+
+        public void Eliminar(string pCodigo)
+        {
+
+        }
+
+        public List<Usuario> ObtenerTodos()
+        {
+            return null;
+        }
+
+        public Usuario ObtenerPorCodigo(string pCodigo)
+        {
+            return null;
+        }
+
+        public IList<Usuario> ObtenerOrdenadosPor(IComparer<Usuario> pComparador)
+        {
+            return null;
+        }
+       
     }
 }
