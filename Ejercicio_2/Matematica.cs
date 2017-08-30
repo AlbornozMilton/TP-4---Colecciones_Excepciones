e5823eb3a5084064de819f22d0a06f14632df47b
Mauri Chamorro <Mauricio-Chamorro@hotmail.com>
2017-08-30 06:30:40 -0300

    Ejercicio 2 sin UnitTest

---
+++ Ejercicio_2/Matematica.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio_2
+{
+    static public class Matematica
+    {
+        static public double Divir(int pDividendo, int pDivisor)
+        {
+
+            try
+            {
+                return pDividendo / pDivisor;
+            }
+            catch (DivideByZeroException pE)
+            {
+
+                throw new DivisionPorCeroException(pE);
+            }
+        }
+    }
+}
