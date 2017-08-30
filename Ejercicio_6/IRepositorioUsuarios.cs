d6efbe767ff7ea90ddf76d5bc83465db31938f3b
Mauri Chamorro <Mauricio-Chamorro@hotmail.com>
2017-08-30 03:38:18 -0300

    antes de manquear

--- Ejercicio_6/IRepositorioUsuarios.cs
+++ Ejercicio_6/IRepositorioUsuarios.cs
@@ -8,13 +8,11 @@ namespace Ejercicio_6
 {
     public interface IRepositoriosUsuarios 
     {
-
         void AgregarUsuario(Usuario pUsuario);
         void Actualizar(Usuario pUsuario);
         void Eliminar(string pCodigo);
         IList<Usuario> ObtenerTodos();
         Usuario ObtenerPorCodigo(string pCodigo);
         IList<Usuario> ObtenerOrdenadoPor(IComparer<Usuario> pUsuario);
-
     }
 }
