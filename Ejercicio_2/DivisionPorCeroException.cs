8664153944912dca1ffe8eef98788d43a6ac2927
Mauri Chamorro <Mauricio-Chamorro@hotmail.com>
2017-08-30 06:30:40 -0300

    Ejercicio 2 sin UnitTest

---
+++ Ejercicio_2/DivisionPorCeroException.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio_2
+{
+    public class DivisionPorCeroException:DivideByZeroException
+    {
+        public DivisionPorCeroException(DivideByZeroException pE) : base("El divisor no puede ser cero", pE)
+        {
+
+        }
+    }
+}
